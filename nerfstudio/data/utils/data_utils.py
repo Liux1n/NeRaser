da1bbd7355dc6c43378304f8d7559eca6fdab661
Liux1n <126062720+Liux1n@users.noreply.github.com>
2023-10-26 22:45:40 +0200

    grayscale

--- nerfstudio/data/utils/data_utils.py
+++ nerfstudio/data/utils/data_utils.py
@@ -27,6 +27,8 @@ def get_image_mask_tensor_from_path(filepath: Path, scale_factor: float = 1.0) -
     Utility function to read a mask image from the given path and return a boolean tensor
     """
     pil_mask = Image.open(filepath)
+    # Convert the image to grayscale
+    pil_mask = pil_mask.convert("L")
     if scale_factor != 1.0:
         width, height = pil_mask.size
         newsize = (int(width * scale_factor), int(height * scale_factor))
